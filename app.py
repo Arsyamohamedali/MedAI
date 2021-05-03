5e85e1b93d4ab503b4b662ca6850f75a54f15350
Arsyamohamedali <70328502+Arsyamohamedali@users.noreply.github.com>
2021-05-03 11:55:48 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+def app():
+  new_model = keras.models.load_model("haemorrhage_modelnorm.h5")
+	st.title("Haemorrhage Detection")
+  uploaded_file_hem = st.file_uploader("Choose a image file", type=['png','jpg','jpeg'])
+	if uploaded_file_hem is not None:
+		image = Image.open(uploaded_file_hem)	
+		size = (128,128)
+		image1=image.resize(size)
+		image1=ImageOps.grayscale(image1)
+		x = np.asarray(image1)
+		x = np.expand_dims(x, axis=0)
+		x=np.reshape(x,(1,128,128,1))
+		x=x/255.0
+		if(new_model.predict_classes(x)[0][0]==1):
+			st.write('***Haemorrhage***')
+			with st.beta_expander('Show Image'):
+				st.image(image,channels='BGR',width=300)
+		else:
+			st.write('***Normal***')
+			with st.beta_expander('Show Image'):
+				st.image(image,channels='BGR',width=300)
+  
